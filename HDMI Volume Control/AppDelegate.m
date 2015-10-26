8a62e4bad87a85f4b15379e148cc0779cb663b59
Ryan <rsul.dev@me.com>
2015-10-26 18:30:14 -0400

    Comment to avoid warning

--- HDMI Volume Control/AppDelegate.m
+++ HDMI Volume Control/AppDelegate.m
@@ -68,7 +68,7 @@
     int keyCode = ((event.data1 & 0xFFFF0000) >> 16);
     int keyFlags = (event.data1 & 0x0000FFFF);
     BOOL keyIsPressed = (((keyFlags & 0xFF00) >> 8)) == 0xA;
-    int keyRepeat = (keyFlags & 0x1);
+//    int keyRepeat = (keyFlags & 0x1);
 
     if (!keyIsPressed) {
         [_cec audioKeyup];
